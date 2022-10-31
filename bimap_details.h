24a6f2e268f1a5871637fb92180ab2b49476d15b
Nomad192 <verenya08@gmail.com>
2022-11-01 01:57:41 +0300

    decor (re-run tests)

--- bimap_details.h
+++ bimap_details.h
@@ -14,15 +14,6 @@ struct key_t : public intrusive::node<Tag> {
   explicit key_t(Key&& key) : key(std::move(key)) {}
 };
 
-//template <typename Base, typename Comparator, typename Tag>
-//struct comparator_t : Comparator {
-//  explicit comparator_t(Comparator &&comp) : Comparator(std::move(comp)) {}
-//  bool operator()(const Base& a,
-//                  const Base& b) const {
-//    return Comparator::operator()(a, b);
-//  }
-//};
-
 template <typename Left, typename Right>
 struct node_t : public key_t<Left, left_tag>, public key_t<Right, right_tag> {
   node_t(Left left, Right right)
